8a619d47510fdb0013c13c5798330135d0d9569a
gje9434 <optimax0@hotmail.com>
2020-06-16 21:03:25 +0100

    create to-do-list functionality

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
+var items = [];
 
-const weekdays = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday"
-];
+app.set("view engine", "ejs");
 
 app.listen(3000, () => {
     console.log("server started");
 })
 
 app.get("/", (req, res) => {
-    let currentDay = new Date();
-    res.render("list", {dayOfTheWeek: weekdays[currentDay.getDay()-1]});
+    let options = {weekday: "long", year: "numeric", month: "long", day: "numeric"};
+    let currentDate = new Date().toLocaleString("en-GB", options);
+    res.render("list", { dayOfTheWeek: currentDate,  items: items});
+})
+
+app.post("/", (req, res) => {
+    let newItem = req.body.newItem;
+    items.push(newItem);
+    res.redirect("/");
 })
